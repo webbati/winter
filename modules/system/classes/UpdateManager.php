e00b58e973eaeed1e06a5bdb4f689c35cf2c05ff
Jack Wilkinson <me@jackwilky.com>
2021-03-13 00:05:39 +0000

    Added support for replacement plugin version updating from current version of replaced plugin

--- modules/system/classes/UpdateManager.php
+++ modules/system/classes/UpdateManager.php
@@ -151,6 +151,9 @@ class UpdateManager
          */
         $plugins = $this->pluginManager->getPlugins();
         foreach ($plugins as $code => $plugin) {
+            if ($plugin->replaces()) {
+                $this->versionManager->replacePlugin($plugin, $code);
+            }
             $this->updatePlugin($code);
         }
 
